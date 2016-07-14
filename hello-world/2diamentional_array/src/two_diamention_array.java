22e1cd6634fc0158b10c67c3b2b746181d483967
PMSawant <prasadsawant023@gmail.com>
2016-07-14 12:47:16 +0530

    ShoppingCartCore

---
+++ hello-world/2diamentional_array/src/two_diamention_array.java
@@ -0,0 +1,27 @@
+
+public class two_diamention_array {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int number[][];
+		number=new int[5][6];
+		int i,j;
+		for(i=0;i<=4;i++)
+		{
+		for(j=0;j<=5;j++)
+		{
+		number[i]=Integer.parse.Int(args[i]);
+		}
+		}
+		for(i=0;i<4;i++)
+		{
+		for(j=0;j<=5;j++)
+		{
+		System.out.println("2diamentional array is"+number[i][j]);
+		}
+		}
+	}
+}
+
+
+
